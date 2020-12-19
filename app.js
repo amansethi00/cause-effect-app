9b5af4eef1390c5efe2433d5a0ca7a226c548e47
Aman Sethi <35420536+amansethi00@users.noreply.github.com>
2020-12-19 23:56:29 +0530

    update index.html and app.js

--- app.js
+++ app.js
@@ -7,7 +7,38 @@ const people = [
     {name: "Ramlal", street: "Street 1", city: "Banglore", state: "Karnataka", country: "India", telephone: "85462145", birthday: "08/08/2000"},
     {name: "Gomuk", street: "...", city: "...", state: "...", country: "...", telephone: "...", birthday: "..."},
     {name: "Noni", street: "...", city: "...", state: "...", country: "...", telephone: "...", birthday: "..."},
-]
+];
+
+
+const names = Array.from(document.querySelectorAll('.names'));
+names.map((item,index)=>{
+    item.innerHTML=people[index].name;
+});
+console.log(people.map(item=>item.name.includes("Ramlal")))
+console.log(names[0].innerText);
+console.log(names.length);
+
+    names.map((item,index)=>
+        item.addEventListener("click",function(){
+            var toDisplay ={
+            name: people[index].name,
+            street: people[index].street,
+            city:people[index].city,
+            state: people[index].state,
+            telephone:people[index].telephone,
+            }
+            para=document.createElement('div');
+            para.innerText=toDisplay.name;
+            summary.appendChild(para);
+        })
+    )
+
+
+
+
+
+
+
 // function makeLink(container,textnode){
 //     a=document.createElement('a');
 //     link = document.createTexNode(textnode);
@@ -16,27 +47,51 @@ const people = [
 //     container.appendChild(a);
 
 // }
-function makeList(){
-     listContainer = document.createElement("ul");
+// function makeList(){
+//      listContainer = document.createElement("ul");
    
-    summary.appendChild(listContainer);
-   
-    people.map(function (item){
+//     summary.appendChild(listContainer);
+  
+//     people.map(function (item,index){
         
-            aLink = document.createElement('a');
-            listElement = document.createElement("li");
+//             aLink = document.createElement('a');
+//             listElement = document.createElement("li");
             
-            aLink.appendChild(document.createTextNode(item.name));
-            aLink.href="hello.com";
-            listElement.appendChild(aLink);
-            listContainer.appendChild(listElement);
+//             aLink.appendChild(document.createTextNode(item.name));
+//             aLink.title=index;
+            
+//             listElement.appendChild(aLink);
+//             listContainer.appendChild(listElement);
        
-    })
+//     })
 
     // for(var i=0;i<people.length;i++){
     //      listElement = document.createElement("li");
     //      listElement.innerHTML = people[i].name;
     //      listContainer.appendChild(listElement);
     // }
-}
-makeList();
+// }
+// function clickHandler(){
+//     // var tit = document.querySelector(aLink).title;
+//     p=document.createElement("p");
+//     var x = document.getElementsByTagName("A")[2].title;
+//     console.log(x);
+//     // document.querySelector('body').appendChild(p);
+// }
+// makeList();
+//  aLink = document.querySelectorAll('a');
+//  console.log(aLink);
+//  aLink.map(function(item){
+//     //  item.addEventListener("click",function(){
+//     //      console.log(item.title);
+//     //  })
+//     console.log(item.index);
+//  })
+//  const choices = Array.from(document.querySelectorAll('li'));
+//  choices.addEventListener("click",function(){
+//     console.log("Hello");
+//  })
+// aLink.addEventListener("click",function (){
+//      console.log(aLink.title);
+        
+// });
